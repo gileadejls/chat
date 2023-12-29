265dfdb313c71ff44d0b445a1a9398e9fcc381e3
Gileadejls <gileadelima16@gmail.com>
2023-12-29 18:12:05 -0300

    primeiro commit

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const http = require('http')
+const socketIO = require('socket.io')
+const path = require('path')
+
+
+
+const app = express()
+const server = http.createServer(app)
+const io = socketIO(server)
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+const messageHistory = []
+
+app.set('views', './src/views')
+app.set('view engine', 'ejs')
+
+
+app.get('/', (req, res)=>{
+    res.render('home')
+})
+
+//CONFIGURANDO O SOCKET.IO
+io.on('connection', (socket) =>{
+    const clientId = socket.id
+    socket.emit('chat history', messageHistory)
+    // console.log('um cliente se conectou')
+    console.log('Cliente Conectado, ID:', clientId)
+    
+    // socket.emit('chat history', messageHistory)
+
+    //logica para lidar com mensagens do cliente
+    socket.on('chat message', (msg)=>{
+        messageHistory.push(msg)
+
+        io.emit('chat history', messageHistory)
+    })
+
+    //logica para lidar com as desconexões do cliente
+    socket.on('disconnect', (event)=>{
+        console.log('um cliente se desconectou')
+        console.log(event)
+    })
+
+
+})
+
+
+
+
+server.listen(3000, ()=>{
+    console.log('Servidor Rodando em http://localhost:3000')
+})
